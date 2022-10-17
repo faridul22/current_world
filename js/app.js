7b693e1d5ab32454f6741abf8d9921fae0078b36
faridul22 <96929890+faridul22@users.noreply.github.com>
2022-10-17 09:54:26 +0600

    display categories title

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const loadCategoriesName = () => {
+    fetch(' https://openapi.programming-hero.com/api/news/categories')
+        .then(response => response.json())
+        .then(data => displayCategoriesName(data.data.news_category))
+        .catch(error => console.log(error))
+}
+loadCategoriesName()
+const displayCategoriesName = (categories) => {
+    const categoresContainer = document.getElementById('categores_container');
+    categories.forEach(category => {
+        const categoiryDiv = document.createElement('div');
+        categoiryDiv.classList.add('categoiry');
+        categoiryDiv.innerHTML = `
+                            <a class="px-2 py-1 rounded" href="">${category.category_name}</a>
+        `;
+        categoresContainer.appendChild(categoiryDiv);
+        console.log(category)
+    });
+    console.log(categories)
+}
