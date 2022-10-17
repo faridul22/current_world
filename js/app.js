f0b632da3e5c07a9f587e482aa5bce2769e9ff4c
faridul22 <96929890+faridul22@users.noreply.github.com>
2022-10-17 16:06:56 +0600

    loding spinner added

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const loadCategoriesName = () => {
         .then(response => response.json())
         .then(data => displayCategoriesName(data.data.news_category))
         .catch(error => console.log(error))
+
 };
 loadCategoriesName();
 
@@ -24,14 +25,19 @@ const loadCategoryId = categoryId => {
     fetch(url)
         .then(response => response.json())
         .then(data => displayNewsByCategory(data.data))
+        .catch(error => console.log(error))
+    toggleSpinner(true);
 };
 
+// display news
 const displayNewsByCategory = newsCategories => {
+    toggleSpinner(true)
     const newsContainer = document.getElementById('news_container');
     newsContainer.textContent = ``;
     newsCategories.forEach(news => {
+        // details text ellipsis
+        news.details = news.details.slice(0, 350);
 
-        news.details = news.details.slice(0, 350)
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
         <div class="card mb-3 my-4">
@@ -40,41 +46,51 @@ const displayNewsByCategory = newsCategories => {
                     <img src="${news.thumbnail_url ? news.thumbnail_url : 'No data found'}" class="img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
-                        <div class="card-body">
+                    <div class="card-body">
                             <h5 class="card-title">${news.title ? news.title : 'No data found'}</h5>
                             <p id="news_details" class="card-text">${news.details ? news.details : 'No data found'}....</p>
-
                             <div class="card-text d-flex justify-content-between align-items-center pt-1">
                                 <div class="d-flex">
-                                   <div>
-                                       <img class="rounded-circle" src="${news.author.img ? news.author.img : 'No data found'}" alt="" width="60" height="60">
-                                   </div>
-                                   <div class="ms-3">
-                                       <h5>${news.author.name ? news.author.name : 'No author founds'}</h5>
-                                       <p class="text-muted">${news.author.published_date ? news.author.published_date : 'No data found'} M</p>
-                                   </div>
-                                </div>
                                 <div>
-                                    <h5><span><i class="fa-regular fa-eye"></i></span> ${news.total_view ? news.total_view : 'No data found'}</h5>
+                                    <img class="rounded-circle" src="${news.author.img ? news.author.img : 'No data found'}" alt="" width="60" height="60">
                                 </div>
-                                <div>
-                                    <span><i class="fa-regular fa-star-half-stroke"></i></span>
-                                    <span><i class="fa-regular fa-star"></i></span>
-                                    <span><i class="fa-regular fa-star"></i></span>
-                                    <span><i class="fa-regular fa-star"></i></span>
-                                    <span><i class="fa-regular fa-star"></i></span>
-                                </div>
-                                <div>
-                                    <p class="text-primary">Show details</p>
+                                <div class="ms-3">
+                                    <h5>${news.author.name ? news.author.name : 'No author founds'}</h5>
+                                    <p class="text-muted">${news.author.published_date ? news.author.published_date : 'No data found'} M</p>
                                 </div>
                             </div>
+                            <div>
+                                <h5><span><i class="fa-regular fa-eye"></i></span> ${news.total_view ? news.total_view : 'No data found'}</h5>
+                            </div>
+                            <div>
+                                <span><i class="fa-regular fa-star-half-stroke"></i></span>
+                                <span><i class="fa-regular fa-star"></i></span>
+                                <span><i class="fa-regular fa-star"></i></span>
+                                <span><i class="fa-regular fa-star"></i></span>
+                                <span><i class="fa-regular fa-star"></i></span>
+                            </div>
+                            <div>
+                                <p class="text-primary">Show details</p>
+                            </div>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>    
         `;
-        newsContainer.appendChild(newsDiv)
+        newsContainer.appendChild(newsDiv);
+        toggleSpinner(false)
         console.log(news)
     })
-    // console.log(newsCategories)
+};
+
+// toggle spinner
+const toggleSpinner = isLoding => {
+    const spinner = document.getElementById('loding_spinner');
+    if (isLoding === true) {
+        spinner.classList.remove('d-none');
+    }
+    else {
+        spinner.classList.add('d-none')
+    }
 }
